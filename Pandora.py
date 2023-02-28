cdabdda31ce24661ae6922bb43b19ba371fc6217
GabrieleConte <gabryconte90@gmail.com>
2023-02-28 22:55:37 +0100

    2nd commit

--- Pandora.py
+++ Pandora.py
@@ -1,12 +1,20 @@
-
-
 class Pandora:
-    def __init__(self, stamina, smax, turns, numDemons):
-        self.startStamina = stamina
+    def __init__(self, stamina, smax):
+        self.stamina = stamina
         self.maxStamina = smax
-        self.turns = turns
-        self.nDemons = numDemons
-        self.punteggio=0
-        self.ricompenseStamina=[]
-        self.ricompenseFrammenti=[]
-        
+        self.ricompenseStamina = []
+        self.ricompenseFrammenti = []
+        self.enemies = []
+        self.frammenti = 0
+
+    def nemicoSconfitto(self, demone):
+        self.enemies.append(demone)
+        self.ricompenseStamina[demone.tr] = self.ricompenseStamina[demone.tr] + demone.sr if self.ricompenseStamina[
+            demone.tr] else demone.sr
+
+        for i in range(demone.na):
+            self.ricompenseFrammenti[i] = self.ricompenseFrammenti + demone.values[i] if self.ricompenseFrammenti[
+                i] else demone.values[i]
+
+    def aggPunteggio(self):
+        pass
--- main.py
+++ main.py
@@ -5,7 +5,9 @@ with open("file.txt") as f:
     frow = f.readline(0).split(" ")
     frow = [eval(i) for i in frow]
     rows = f.readlines()
-    game = Pandora(int(frow[0]), int(frow[1]), int(frow[2]), int(frow[3]))
+    pandora = Pandora(frow[0], frow[1])
+    turns = frow[2]
+    ndemons = frow[3]
     demons = []
     for i in range(1, len(rows)):
         statsDemone = rows[i].split(" ")
@@ -15,3 +17,16 @@ with open("file.txt") as f:
             x.append(statsDemone[j + 3])
         demone = Demon(statsDemone[0], statsDemone[1], statsDemone[2], statsDemone[3], x)
         demons.append(demone)
+    i = 0
+    while turns >= 0:
+        turns -= 1
+        if demons[i].sc > pandora.stamina:
+            turns -= 1
+            i += 1
+        else:
+            pass
+        ##implementare il modo in cui pandora sceglie i nemici
+        # se un nemico non è affrontabile, aspetta un turno
+        # se è affrontabile, scala la stamina a pandora e aggiorna le ricompense del demone
+        # alla fine di ogni turno aggiungi le ricompense dai vettori di ricompense
+        # e aggiorna le ricompense sulla stamina e sui fragments##
