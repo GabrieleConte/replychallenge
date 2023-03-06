304d0d1faa7f6311153c394e741af173834bbf8b
GabrieleConte <gabryconte90@gmail.com>
2023-03-06 23:13:11 +0100

    5th

--- Pandora.py
+++ Pandora.py
@@ -2,31 +2,35 @@ class Pandora:
     def __init__(self, stamina, smax):
         self.stamina = stamina
         self.maxStamina = smax
-        self.ricompenseStamina = []
-        self.ricompenseFrammenti = []
+        self.ricompenseStamina = [0]
+        self.ricompenseFrammenti = [0]
         self.frammenti = 0
 
     def nemicoSconfitto(self, demone):
-        if demone.tr<len(self.ricompenseStamina):
-            self.ricompenseStamina[demone.tr-1] = self.ricompenseStamina[demone.tr-1] + demone.sr if self.ricompenseStamina[
-                demone.tr-1] else demone.sr
+        if demone.tr < len(self.ricompenseStamina):
+            self.ricompenseStamina[demone.tr - 1] = self.ricompenseStamina[demone.tr - 1] + demone.sr if \
+            self.ricompenseStamina[
+                demone.tr - 1] else demone.sr
         else:
-            for _ in range(demone.tr-len(self.ricompenseStamina)):
+            for _ in range(demone.tr - len(self.ricompenseStamina)):
                 self.ricompenseStamina.append(0)
-            self.ricompenseStamina[demone.tr - 1] = self.ricompenseStamina[demone.tr - 1] + demone.sr if self.ricompenseStamina[demone.tr - 1] else demone.sr
+            self.ricompenseStamina[demone.tr - 1] = self.ricompenseStamina[demone.tr - 1] + demone.sr if \
+            self.ricompenseStamina[demone.tr - 1] else demone.sr
 
-        if len(self.ricompenseFrammenti)>demone.na:
-            for _ in range(demone.na-len(self.ricompenseFrammenti)):
+        if len(self.ricompenseFrammenti) < demone.na:
+            for _ in range(demone.na - len(self.ricompenseFrammenti)):
                 self.ricompenseFrammenti.append(0)
         for i in range(demone.na):
             self.ricompenseFrammenti[i] = self.ricompenseFrammenti[i] + demone.values[i] if self.ricompenseFrammenti[
                 i] else demone.values[i]
-        self.stamina-= demone.sc
+        self.stamina -= demone.sc
 
     def aggPunteggio(self):
-        self.frammenti += self.ricompenseFrammenti.pop(0) if self.ricompenseFrammenti[0] else self.frammenti
+        self.frammenti = self.frammenti + self.ricompenseFrammenti.pop() if self.ricompenseFrammenti[
+            0] else self.frammenti
 
     def aggStamina(self):
-        self.stamina= self.stamina + self.ricompenseStamina.pop(0) if self.ricompenseStamina[0] else self.stamina
-        if self.stamina>20:
-            self.stamina=20
+        print(self.ricompenseStamina)
+        self.stamina = self.stamina + self.ricompenseStamina.pop() if self.ricompenseStamina[0] else self.stamina
+        if self.stamina > self.maxStamina:
+            self.stamina = self.maxStamina
--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from Demon import Demon
 from Pandora import Pandora
 
 with open("file.txt") as f:
-    frow = f.readline(0).split(" ")
+    frow = f.readline().split(" ")
     frow = [eval(i) for i in frow]
     rows = f.readlines()
     pandora = Pandora(frow[0], frow[1])
     turns = frow[2]
     ndemons = frow[3]
     demons = []
-    for i in range(1, len(rows)):
+    for i in range(0, len(rows)):
         statsDemone = rows[i].split(" ")
         statsDemone = [eval(i) for i in statsDemone]
         x = []
@@ -17,27 +17,31 @@ with open("file.txt") as f:
             x.append(statsDemone[j + 3])
         demone = Demon(statsDemone[0], statsDemone[1], statsDemone[2], statsDemone[3], x)
         demons.append(demone)
+    print(demons[0].sc)
     i = 0
-    output=[]
-    f = open("result.txt", "x")
+    output = []
     while turns > 0:
         pandora.aggPunteggio()
         pandora.aggStamina()
         if pandora.stamina > 0:
             flag = True
             i = 0
-            while flag and i < ndemons:
+            while flag and i <= len(demons) - 1:
                 if demons[i].sc < pandora.stamina and i not in output:
+                    print(demons[i].sc)
                     flag = False
                     pandora.nemicoSconfitto(demons[i])
+                    print(pandora.stamina)
                     output.append(i)
-                    f.write(f"{i}\n")
                 else:
                     i += 1
         turns -= 1
+n = open('output.txt', 'w')
+for index in output:
+    n.write(f"{index}\n")
 
-        ##implementare il modo in cui pandora sceglie i nemici
-        # se un nemico non è affrontabile, aspetta un turno
-        # se è affrontabile, scala la stamina a pandora e aggiorna le ricompense del demone
-        # alla fine di ogni turno aggiungi le ricompense dai vettori di ricompense
-        # e aggiorna le ricompense sulla stamina e sui fragments##
+    ##implementare il modo in cui pandora sceglie i nemici
+    # se un nemico non è affrontabile, aspetta un turno
+    # se è affrontabile, scala la stamina a pandora e aggiorna le ricompense del demone
+    # alla fine di ogni turno aggiungi le ricompense dai vettori di ricompense
+    # e aggiorna le ricompense sulla stamina e sui fragments##
